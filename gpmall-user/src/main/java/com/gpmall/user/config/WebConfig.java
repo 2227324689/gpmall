62485377cf2ed850c2bd65d69390c07c1747760e
jerry_sk <jerry_sk>
2019-08-10 19:34:50 +0800

    fork 提交测试

--- gpmall-user/src/main/java/com/gpmall/user/config/WebConfig.java
+++ gpmall-user/src/main/java/com/gpmall/user/config/WebConfig.java
@@ -9,7 +9,7 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 /**
  * 腾讯课堂搜索【咕泡学院】
  * 官网：www.gupaoedu.com
- * 风骚的Mic 老师
+ * 风骚的Mic 老师1111
  * create-date: 2019/7/22-19:01
  */
 @Configuration
