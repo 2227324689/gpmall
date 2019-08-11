6fd97d88b16edc872a683465477114f2487b2d23
张明 <wb.zm2017@mesg.corp.netease.com>
2019-08-11 13:56:07 +0800

    添加公共分布式锁组件 author:851555250@qq.com

---
+++ gpmall-commons/commons-lock/src/main/java/com/gpmall/commons/lock/extension/ConcurrentHashSet.java
@@ -0,0 +1,56 @@
+package com.gpmall.commons.lock.extension;
+
+import java.util.AbstractSet;
+import java.util.Iterator;
+import java.util.Set;
+import java.util.concurrent.ConcurrentHashMap;
+
+public class ConcurrentHashSet<E> extends AbstractSet<E> implements Set<E>, java.io.Serializable {
+
+    private static final long serialVersionUID = -8672117787651310382L;
+
+    private static final Object PRESENT = new Object();
+
+    private final ConcurrentHashMap<E, Object> map;
+
+    public ConcurrentHashSet() {
+        map = new ConcurrentHashMap<E, Object>();
+    }
+
+    public ConcurrentHashSet(int initialCapacity) {
+        map = new ConcurrentHashMap<E, Object>(initialCapacity);
+    }
+
+
+    public Iterator<E> iterator() {
+        return map.keySet().iterator();
+    }
+
+    public int size() {
+        return map.size();
+    }
+
+    public boolean isEmpty() {
+        return map.isEmpty();
+    }
+
+    public boolean contains(Object o) {
+        return map.containsKey(o);
+    }
+
+
+    public boolean add(E e) {
+        return map.put(e, PRESENT) == null;
+    }
+
+
+    public boolean remove(Object o) {
+        return map.remove(o) == PRESENT;
+    }
+
+
+    public void clear() {
+        map.clear();
+    }
+
+}
