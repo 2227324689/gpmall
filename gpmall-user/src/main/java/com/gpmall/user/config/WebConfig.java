a4497ea04cf2fe384c55bf6b9720935b1dad67c3
cxp <945098753@qq.com>
2019-08-14 12:40:57 +0800

    test

--- gpmall-user/src/main/java/com/gpmall/user/config/WebConfig.java
+++ gpmall-user/src/main/java/com/gpmall/user/config/WebConfig.java
@@ -23,6 +23,7 @@ public class WebConfig implements WebMvcConfigurer {
     public void addInterceptors(InterceptorRegistry registry) {
         registry.addInterceptor(tokenIntercepter())
                 .addPathPatterns("/user/**")
+
                 .excludePathPatterns("/error");
     }
 }
