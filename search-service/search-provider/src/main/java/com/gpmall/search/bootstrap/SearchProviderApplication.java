b79d4b8c24d3c1ff99811f9f22411f6452880755
wangzhong <wz@weiyian.com>
2019-08-14 20:46:27 +0800

    完善订单删除和取消 510172336@qq.com

--- search-service/search-provider/src/main/java/com/gpmall/search/bootstrap/SearchProviderApplication.java
+++ search-service/search-provider/src/main/java/com/gpmall/search/bootstrap/SearchProviderApplication.java
@@ -5,6 +5,7 @@ import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
+import org.springframework.data.elasticsearch.repository.config.EnableElasticsearchRepositories;
 
 /**
  * 搜索服务启动类
@@ -15,6 +16,7 @@ import org.springframework.context.annotation.ComponentScan;
 @SpringBootApplication
 @ComponentScan(basePackages = "com.gpmall.search.repository")
 @MapperScan(basePackages = "com.gpmall.search.mapper")
+@EnableElasticsearchRepositories(basePackages = "com.gpmall.search.repository")
 public class SearchProviderApplication {
 
 	public static void main(String[] args) {
