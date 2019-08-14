dd385b5a77e8fe33519dcef9db8b4a3b8de305dd
to_be_continued <tufeng1992@163.com>
2019-08-14 23:53:05 +0800

    1.提交修改为tkmapper后涉及接口的单元测试类。

--- shopping-service/shopping-provider/src/test/java/com/gpmall/shopping/bootstrap/ShoppingProviderApplicationTests.java
+++ shopping-service/shopping-provider/src/test/java/com/gpmall/shopping/bootstrap/ShoppingProviderApplicationTests.java
@@ -1,19 +1,75 @@
 package com.gpmall.shopping.bootstrap;
 
+import com.gpmall.shopping.*;
+import com.gpmall.shopping.dto.AddCartRequest;
+import com.gpmall.shopping.dto.AllProductCateRequest;
+import com.gpmall.shopping.dto.ProductDetailRequest;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import java.io.IOException;
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
 public class ShoppingProviderApplicationTests {
 
-    @Test
-    public void contextLoads() {
+    @Autowired
+    private ICartService cartService;
 
+
+    @Test
+    public void testCartService() throws IOException {
+        AddCartRequest request = new AddCartRequest();
+        request.setItemId(100023501L);
+        request.setUserId(123L);
+        cartService.addToCart(request);
+        System.in.read();
     }
 
+    @Autowired
+    private IContentService contentService;
+
+    @Test
+    public void testContentService() throws IOException {
+        contentService.queryNavList();
+        System.in.read();
+    }
+
+    @Autowired
+    private IHomeService homeService;
+    @Test
+    public void testHomeService() throws IOException {
+        homeService.homepage();
+        System.in.read();
+    }
+
+    @Autowired
+    private IProductCateService productCateService;
+
+    @Test
+    public void testProductCateService() throws IOException {
+        AllProductCateRequest request = new AllProductCateRequest();
+        request.setSort("1");
+        productCateService.getAllProductCate(request);
+        System.in.read();
+    }
+
+    @Autowired
+    private IProductService productService;
+
+    @Test
+    public void testProductService() throws IOException {
+        ProductDetailRequest productDetailRequest = new ProductDetailRequest();
+        productDetailRequest.setId(100023501L);
+        productService.getProductDetail(productDetailRequest);
+
+        // ----------------------------------------------------------------------------
+
+//        productService.getRecommendGoods();
+
+        System.in.read();
+    }
 }
