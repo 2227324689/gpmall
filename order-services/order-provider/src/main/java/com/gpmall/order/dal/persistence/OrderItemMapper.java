2573c0548f39cd43ab75db14e1adeee9e40c0732
jzx <937345232@qq.com>
2019-08-14 23:46:28 +0800

    优化order-provider mybatis

--- order-services/order-provider/src/main/java/com/gpmall/order/dal/persistence/OrderItemMapper.java
+++ order-services/order-provider/src/main/java/com/gpmall/order/dal/persistence/OrderItemMapper.java
@@ -1,32 +1,11 @@
 package com.gpmall.order.dal.persistence;
 
+import com.gpmall.commons.tool.tkmapper.TkMapper;
 import com.gpmall.order.dal.entitys.OrderItem;
-import com.gpmall.order.dal.entitys.OrderItemExample;
-import java.util.List;
 import org.apache.ibatis.annotations.Param;
 
-public interface OrderItemMapper {
-    long countByExample(OrderItemExample example);
+import java.util.List;
 
-    int deleteByExample(OrderItemExample example);
-
-    int deleteByPrimaryKey(String id);
-
-    int insert(OrderItem record);
-
-    int insertSelective(OrderItem record);
-
-    List<OrderItem> selectByExample(OrderItemExample example);
-
-    OrderItem selectByPrimaryKey(String id);
-
-    int updateByExampleSelective(@Param("record") OrderItem record, @Param("example") OrderItemExample example);
-
-    int updateByExample(@Param("record") OrderItem record, @Param("example") OrderItemExample example);
-
-    int updateByPrimaryKeySelective(OrderItem record);
-
-    int updateByPrimaryKey(OrderItem record);
-
-    int deleteItemByOrderId(@Param("orderId") String orderId);
+public interface OrderItemMapper extends TkMapper<OrderItem> {
+    List<OrderItem> queryByOrderId(@Param("orderId") String orderId);
 }
