85dc36ded79a8353eaf6d06cb1d0a2d9ea43ff4d
leon <1311552105@qq.com>
2019-08-15 16:47:49 +0800

    搜索获取热词接口添加

--- search-service/search-provider/src/main/java/com/gpmall/search/bootstrap/SearchProviderApplication.java
+++ search-service/search-provider/src/main/java/com/gpmall/search/bootstrap/SearchProviderApplication.java
@@ -1,7 +1,6 @@
 package com.gpmall.search.bootstrap;
 
 
-//import org.mybatis.spring.annotation.MapperScan;
 import org.mybatis.spring.annotation.MapperScan;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
