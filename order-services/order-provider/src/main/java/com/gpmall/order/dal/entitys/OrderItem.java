852d10e7b4fe9849a581863548d39ed26ae5cce1
zhangzhenguo <562035092@qq.com>
2019-08-18 11:23:31 +0800

    冲突

--- order-services/order-provider/src/main/java/com/gpmall/order/dal/entitys/OrderItem.java
+++ order-services/order-provider/src/main/java/com/gpmall/order/dal/entitys/OrderItem.java
@@ -1,189 +1,105 @@
 package com.gpmall.order.dal.entitys;
 
+
+
+import lombok.Data;
+
+
+
 import java.math.BigDecimal;
+
 import javax.persistence.*;
 
+
+
 @Table(name = "tb_order_item")
+
+@Data
+
 public class OrderItem {
+
     @Id
+
     private String id;
 
+
+
     /**
+
      * 商品id
+
      */
+
     @Column(name = "item_id")
+
     private String itemId;
 
+
+
     /**
+
      * 订单id
+
      */
+
     @Column(name = "order_id")
+
     private String orderId;
 
+
+
     /**
+
      * 商品购买数量
+
      */
+
     private Integer num;
 
+
+
     /**
+
      * 商品标题
+
      */
+
     private String title;
 
+
+
     /**
+
      * 商品单价
+
      */
-    private BigDecimal price;
+
+    private Double price;
+
+
 
     /**
+
      * 商品总金额
+
      */
+
     @Column(name = "total_fee")
-    private BigDecimal totalFee;
+
+    private Double totalFee;
+
+
 
     /**
+
      * 商品图片地址
+
      */
+
     @Column(name = "pic_path")
+
     private String picPath;
 
-    /**
-     * @return id
-     */
-    public String getId() {
-        return id;
-    }
 
-    /**
-     * @param id
-     */
-    public void setId(String id) {
-        this.id = id;
-    }
 
-    /**
-     * 获取商品id
-     *
-     * @return item_id - 商品id
-     */
-    public String getItemId() {
-        return itemId;
-    }
-
-    /**
-     * 设置商品id
-     *
-     * @param itemId 商品id
-     */
-    public void setItemId(String itemId) {
-        this.itemId = itemId;
-    }
-
-    /**
-     * 获取订单id
-     *
-     * @return order_id - 订单id
-     */
-    public String getOrderId() {
-        return orderId;
-    }
-
-    /**
-     * 设置订单id
-     *
-     * @param orderId 订单id
-     */
-    public void setOrderId(String orderId) {
-        this.orderId = orderId;
-    }
-
-    /**
-     * 获取商品购买数量
-     *
-     * @return num - 商品购买数量
-     */
-    public Integer getNum() {
-        return num;
-    }
-
-    /**
-     * 设置商品购买数量
-     *
-     * @param num 商品购买数量
-     */
-    public void setNum(Integer num) {
-        this.num = num;
-    }
-
-    /**
-     * 获取商品标题
-     *
-     * @return title - 商品标题
-     */
-    public String getTitle() {
-        return title;
-    }
-
-    /**
-     * 设置商品标题
-     *
-     * @param title 商品标题
-     */
-    public void setTitle(String title) {
-        this.title = title;
-    }
-
-    /**
-     * 获取商品单价
-     *
-     * @return price - 商品单价
-     */
-    public BigDecimal getPrice() {
-        return price;
-    }
-
-    /**
-     * 设置商品单价
-     *
-     * @param price 商品单价
-     */
-    public void setPrice(BigDecimal price) {
-        this.price = price;
-    }
-
-    /**
-     * 获取商品总金额
-     *
-     * @return total_fee - 商品总金额
-     */
-    public BigDecimal getTotalFee() {
-        return totalFee;
-    }
-
-    /**
-     * 设置商品总金额
-     *
-     * @param totalFee 商品总金额
-     */
-    public void setTotalFee(BigDecimal totalFee) {
-        this.totalFee = totalFee;
-    }
-
-    /**
-     * 获取商品图片地址
-     *
-     * @return pic_path - 商品图片地址
-     */
-    public String getPicPath() {
-        return picPath;
-    }
-
-    /**
-     * 设置商品图片地址
-     *
-     * @param picPath 商品图片地址
-     */
-    public void setPicPath(String picPath) {
-        this.picPath = picPath;
-    }
 }
