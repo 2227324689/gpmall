33f7225c14559cf3ea01eb036ea346ce03d58596
hps <1072685605@qq.com>
2019-08-18 22:17:26 +0800

    1、添加删除商品评价接口 2、添加商品评价置顶接口 3、添加商品评价审核接口

--- comment-service/comment-api/src/main/java/com/gpmall/comment/ICommentService.java
+++ comment-service/comment-api/src/main/java/com/gpmall/comment/ICommentService.java
@@ -37,4 +37,25 @@ public interface ICommentService {
      * @return
      */
     TotalCommentResponse totalComment(TotalCommentRequest request);
+
+    /**
+     * 删除评价
+     * @param request
+     * @return
+     */
+    DeleteCommentResponse deleteComment(DeleteCommentRequest request);
+
+    /**
+     * 将商品评价置顶
+     * @param request
+     * @return
+     */
+    TopCommentResponse topComment(TopCommentRequest request);
+
+    /**
+     * 商品评价审核
+     * @param request
+     * @return
+     */
+    AuditCommentResponse auditComment(AuditCommentRequest request);
 }
---
+++ comment-service/comment-api/src/main/java/com/gpmall/comment/dto/DeleteCommentResponse.java
@@ -0,0 +1,13 @@
+package com.gpmall.comment.dto;
+
+import com.gpmall.commons.result.AbstractResponse;
+import lombok.Data;
+
+/**
+ * @author heps
+ * @date 2019/8/18 8:34
+ * 删除某个评价返回结果
+ */
+@Data
+public class DeleteCommentResponse extends AbstractResponse {
+}
