dcc7570bb7d96b9b22866912c594e3ef31870acd
zzz <zzz59687310@163.com>
2019-08-22 15:38:02 +0800

    add swagger for gpmall shopping

--- gpmall-shopping/src/main/java/com/gpmall/shopping/controller/ProductCateController.java
+++ gpmall-shopping/src/main/java/com/gpmall/shopping/controller/ProductCateController.java
@@ -7,6 +7,9 @@ import com.gpmall.shopping.constants.ShoppingRetCode;
 import com.gpmall.shopping.dto.AllProductCateRequest;
 import com.gpmall.shopping.dto.AllProductCateResponse;
 import com.gpmall.user.annotation.Anoymous;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiImplicitParam;
+import io.swagger.annotations.ApiOperation;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.dubbo.config.annotation.Reference;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -21,21 +24,21 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RestController
 @RequestMapping("/shopping")
+@Api(tags = "ProductCateController", description = "商品种类控制层")
 public class ProductCateController {
     @Reference(timeout = 3000)
     IProductCateService productCateService;
 
     @Anoymous
     @GetMapping("/categories")
-    public ResponseData allProductCateList(@RequestParam(
-            value = "sort",
-            required = false,
-            defaultValue = "1")String sort){
+    @ApiOperation("列举所有商品种类")
+    @ApiImplicitParam(name = "sort", value = "是否排序", paramType = "query")
+    public ResponseData allProductCateList(@RequestParam(value = "sort", required = false, defaultValue = "1") String sort) {
         AllProductCateRequest request = new AllProductCateRequest();
         request.setSort(sort);
-        AllProductCateResponse response= productCateService.getAllProductCate(request);
+        AllProductCateResponse response = productCateService.getAllProductCate(request);
 
-        if(response.getCode().equals(ShoppingRetCode.SUCCESS.getCode())) {
+        if (response.getCode().equals(ShoppingRetCode.SUCCESS.getCode())) {
             return new ResponseUtil().setData(response.getProductCateDtoList());
         }
         return new ResponseUtil().setErrorMsg(response.getMsg());
