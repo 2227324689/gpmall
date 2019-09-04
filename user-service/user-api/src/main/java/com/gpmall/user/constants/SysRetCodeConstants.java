a2ae3cb32af98f4d9f1f1ca578af518650ecc5e8
jerry_sk <jerry_sk>
2019-09-04 14:24:12 +0800

    邮件激活功能的实现，发送发注册信息给kafka，kafka消费信息发送邮件；

--- user-service/user-api/src/main/java/com/gpmall/user/constants/SysRetCodeConstants.java
+++ user-service/user-api/src/main/java/com/gpmall/user/constants/SysRetCodeConstants.java
@@ -16,6 +16,9 @@ public enum SysRetCodeConstants {
     USERNAME_ALREADY_EXISTS             ("003003","用户名已存在"),
     USER_REGISTER_FAILED                ("003004","注册失败，请联系管理员"),
     KAPTCHA_CODE_ERROR                  ("003005","验证码不正确"),
+    USER_ISVERFIED_ERROR                ("003006","用户名尚未激活"),
+    USER_REGISTER_VERIFY_FAILED         ("003007","用户注册失败插入验证数据失败"),
+    USER_INFOR_INVALID                  ("003004","用户信息不合法"),
 
     REQUEST_FORMAT_ILLEGAL              ("003060", "请求数据格式非法"),
     REQUEST_IP_ILLEGAL                  ("003061", "请求IP非法"),
@@ -28,7 +31,9 @@ public enum SysRetCodeConstants {
     PERMISSION_DENIED                   ("003091", "权限不足"),
     DB_EXCEPTION                        ("003097", "数据库异常"),
     SYSTEM_TIMEOUT                      ("003098", "系统超时"),
-    SYSTEM_ERROR                        ("003099", "系统错误");
+    SYSTEM_ERROR                        ("003099", "系统错误"),
+
+    USERVERIFY_INFOR_INVALID            ("003200", "用户注册验证验证信息不合法");
 
     private String code;
     private String message;
